cc78f745360c9295922fbe71a1323993dcff4949
sagnolkamdem <sagnolkamdem721@gmail.com>
2022-12-10 17:37:36 +0100

    :lipstick: sidebar
    sidebar

--- shared/layout/navbar.tsx
+++ shared/layout/navbar.tsx
@@ -3,20 +3,25 @@ import HeaderLogo from "../components/logo/headerLogo";
 
 const Navbar = () => {
     return ( 
-        <header className="flex flex-row bg-white px-8 py-2.5 shadow justify-between">
+        <header className="flex flex-row bg-white md:px-8 px-4 py-2.5 shadow justify-between">
 
-            <div className="flex gap-10">
+            <div className="flex md:gap-10 gap-8 items-center">
 
-                <span className="w-20 h-8">
-                    <HeaderLogo />
+                <span className="h-full flex items-center justify-center">
+                    <HeaderLogo props="md:block hidden" />
+                    <span className="md:hidden flex h-full justify-center items-center">
+                        <svg className="w-6 h-6 stroke-slate-500" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M3 12H21M3 6H21M9 18H21" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                    </span>
                 </span>
 
-                <SearchInput placeholder="Search" name="search" class="w-72"/>
+                <SearchInput placeholder="Search" name="search" class="md:w-72 w-60"/>
 
             </div>
 
-            <div className="flex items-center divide-x divide-gray-200">
-                <button className="flex items-center gap-4 px-4 text-slate-600 text-sm font-normal">
+            <div className="flex items-center md:divide-x divide-gray-200">
+                <button className="md:flex hidden items-center gap-4 px-4 text-slate-600 text-sm font-normal">
                     <span aria-hidden="true">
                         <svg className="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g opacity="0.75">
@@ -32,7 +37,7 @@ const Navbar = () => {
                             <path d="M9.35419 21C10.0593 21.6224 10.9856 22 12 22C13.0145 22 13.9407 21.6224 14.6458 21M18 8C18 6.4087 17.3679 4.88258 16.2427 3.75736C15.1174 2.63214 13.5913 2 12 2C10.4087 2 8.8826 2.63214 7.75738 3.75736C6.63216 4.88258 6.00002 6.4087 6.00002 8C6.00002 11.0902 5.22049 13.206 4.34968 14.6054C3.61515 15.7859 3.24788 16.3761 3.26134 16.5408C3.27626 16.7231 3.31488 16.7926 3.46179 16.9016C3.59448 17 4.19261 17 5.38887 17H18.6112C19.8074 17 20.4056 17 20.5382 16.9016C20.6852 16.7926 20.7238 16.7231 20.7387 16.5408C20.7522 16.3761 20.3849 15.7859 19.6504 14.6054C18.7795 13.206 18 11.0902 18 8Z" stroke="#64748B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </button>
-                    <button>
+                    <button className="md:block hidden">
                         <img className="w-8 h-8 rounded-full" src="/images/avatar.png" alt="profile photo" />
                     </button>
                 </div>
